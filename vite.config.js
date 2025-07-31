0944dd1d0dc7d53d7cee642692de0dea3a29174c
Fatma R <anthoschichequs@gmail.com>
2025-07-31 12:34:25 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        cors: true,
+        host: true,
+        hmr: {
+            host: process.env.VITE_HOST || 'vite.elkdesignn.test',
+            clientPort: 443,
+            protocol: 'wss',
+        },
+        allowedHosts: ['localhost', '127.0.0.1', 'elkdesignn.test', 'vite.elkdesignn.test'],
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
-
         }),
     ],
 });
